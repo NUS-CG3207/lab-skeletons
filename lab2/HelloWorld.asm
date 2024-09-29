fc31ebc48fdee5b74122f48aaa829060853b66f2
rustyrisc <86955414+rustyrisc@users.noreply.github.com>
2024-09-30 01:55:20 +0800

    some cleanup

--- lab2/HelloWorld.asm
+++ lab2/HelloWorld.asm
@@ -10,7 +10,7 @@
 #--		(vi) retain this notice in this file and any files derived from this.
 #----------------------------------------------------------------------------------
 
-# This sample program prints "Welcome to CG3207" in response to "A\r" (A+Enter) received from Console. There should be sufficient time gap between the press of 'A' and '\r'
+# This sample program prints "Welcome to CG3207" in response to "A\r" (A+Enter) received from Console. There should be a sufficient time gap between the press of 'A' and '\r'
 # if the processor is run at a low freq.
 
 .eqv LSB_MASK 0xFF
@@ -19,9 +19,9 @@
 .text	## IROM segment 0x00000000-0x000001FC
 # Total number of instructions should not exceed 128 (127 excluding the last line 'halt B halt').
 
-# Note : see the wiki regarding the pseudoinstructions li and la. 
+# Note: see the wiki regarding the pseudoinstructions li and la. 
 # Pseudoinstructions may be implemented using more than one actual instruction. See the assembled code in the Execute tab of RARS
-# You can also use the actual register numbers directly. For example, instead of s6, you can write x22
+# You can also use the actual register numbers directly. For example, instead of s6 (ABI name), you can write x22, but the former is preferred
 
 main:   
 	li s6, LSB_MASK			# A mask for extracting out the LSB to check for '\0'
@@ -74,10 +74,10 @@ NEXTCHAR:
 	sub t2, t2, t1		# decrement the loop counter
 	bnez t2, NEXTCHAR	# check and print the next character in the word
 	addi a0, a0, 4		# point to next word (4 characters)
-	jal PRINT_S
+	j PRINT_S		# start printing the next word
 halt:	
-	jal halt		# infinite loop to halt computation. A program should not "terminate" without an operating system to return control to
-				# keep halt: jal halt as the last line of your code, though not strictly necessary if there is an infinite loop somewhere.
+	j halt			# infinite loop to halt computation. A program should not "terminate" without an operating system to return control to
+				# keep halt: j halt as the last line of your code, though not strictly necessary if there is an infinite loop somewhere.
 				
 # ------- <code memory (ROM mapped to Instruction Memory) ends>			
 				
@@ -114,13 +114,13 @@ DRAM:
 # ------- <memory-mapped input-output (peripherals) begins>
 .align 9 ## MMIO segment. 0x00002400-0x00002418
 MMIO:
-LEDS: .word 0x0			# 0x00002400	# Address of LEDs. //volatile unsigned int * LEDS = (unsigned int*)0x00000C00#  
-DIPS: .word 0x0			# 0x00002404	# Address of DIP switches. //volatile unsigned int * DIPS = (unsigned int*)0x00000C04#
-PBS: .word 0x0			# 0x00002408	# Address of Push Buttons. Used only in Lab 2
-CONSOLE: .word 0x0		# 0x0000240C	# Address of UART. Used only in Lab 2 and later
-CONSOLE_IN_valid: .word 0x0	# 0x00002410	# Address of UART. Used only in Lab 2 and later
-CONSOLE_OUT_ready: .word 0x0	# 0x00002414	# Address of UART. Used only in Lab 2 and later
-SEVENSEG: .word	0x0		# 0x00002418	# Address of 7-Segment LEDs. Used only in Lab 2 and later
+LEDS: .word 0x0			# 0x00002400	# Location simulating LEDs. //volatile unsigned int* LEDS = (unsigned int*)0x00002400#  
+DIPS: .word 0x0			# 0x00002404	# Location simulating DIP switches. //volatile unsigned int* DIPS = (unsigned int*)0x00002404#
+PBS: .word 0x0			# 0x00002408	# Location simulating Push Buttons. Used only in Lab 2
+CONSOLE: .word 0x0		# 0x0000240C	# Location simulating UART. Used only in Lab 2 and later
+CONSOLE_IN_valid: .word 0x0	# 0x00002410	# Location simulating UART input valid flag. Used only in Lab 2 and later
+CONSOLE_OUT_ready: .word 0x0	# 0x00002414	# Location simulating UART ready for output flag. Used only in Lab 2 and later
+SEVENSEG: .word	0x0		# 0x00002418	# Location simulating 7-Segment LEDs. Used only in Lab 2 and later
 
 # ------- <memory-mapped input-output (peripherals) ends>
 
